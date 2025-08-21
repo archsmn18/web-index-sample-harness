c01d3ffe78d07c92740fc27633c5eef0a4b868b2
Archisman Mukhopadhyay <108763234+archsmn18@users.noreply.github.com>
2025-08-21 13:35:26 +0530

    Delete scripts directory

--- scripts/install_httpd.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-if command -v dnf >/dev/null 2>&1; then
-  dnf install -y httpd
-else
-  yum install -y httpd
-fi
-systemctl stop httpd || true
-mkdir -p /var/www/html
--- scripts/restart_httpd.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-set -e
-systemctl enable httpd
-systemctl restart httpd
